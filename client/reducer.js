c911137ef97d427cbb34c9e16495f00a30ae4725
foxdonut <fdaoud@fastmail.com>
2015-12-12 16:21:26 -0500

    Working on lens version.

--- client/reducer.js
+++ client/reducer.js
@@ -1,7 +1,9 @@
+import {compose, lensIndex, lensProp, set} from "ramda";
+
 const reducer = function(state, action) {
   const move = action.payload;
 
-  // Account for initial action
+  // Account for initial action, which is not a move
   if (!move) {
     return state;
   }
@@ -12,25 +14,27 @@ const reducer = function(state, action) {
     (token.player === 1 && move.row === token.row) ||
     (token.player === 2 && move.col === token.col);
 
-  let addedPoints = [0, 0];
-  addedPoints[token.player - 1] = move.number;
-
   if (validMove) {
-    const nextPlayer = token.player === 1 ? 2 : 1;
+    let addedPoints = [0, 0];
+    addedPoints[token.player - 1] = move.number;
+
+    const nextPlayer = 2 - ((token.player + 1) % 2);
     const rows = state.rows;
 
-    const row = rows[move.row].cols;
-    row[move.col] = {player: nextPlayer}; // set
+    const rowLens = compose(lensIndex(move.col),  lensProp("cols"), lensIndex(move.row));
+    const colLens = compose(lensIndex(token.col), lensProp("cols"), lensIndex(token.row));
+    const nextRows = compose(set(colLens, {}), set(rowLens, {player: nextPlayer}))(rows);
+    const status = "Player " + token.player + " scored " + move.number + ". Player " + nextPlayer + "'s turn.";
 
-    rows[token.row].cols[token.col] = {}; // set
-
-    return state.set("token", {player: nextPlayer, row: move.row, col: move.col}).
-      set("status", "Player " + token.player + " scored " + move.number + ". Player " + nextPlayer + "'s turn.").
-      set("points", [state.points[0] + addedPoints[0], state.points[1] + addedPoints[1]]).
-      set("rows", rows);
+    return compose(
+      set(lensProp("token"), {player: nextPlayer, row: move.row, col: move.col}),
+      set(lensProp("status"), status),
+      set(lensProp("points"), [state.points[0] + addedPoints[0], state.points[1] + addedPoints[1]]),
+      set(lensProp("rows"), nextRows)
+    )(state);
   }
   else {
-    return state.set("status", "Invalid move!");
+    return set(lensProp("status"), "Invalid move!", state);
   }
 };
 
